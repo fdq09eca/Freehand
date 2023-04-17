64afc97ecb45ef17a5cf3170b0785c35c936670a
ChrisLam <fdq09eca@gmail.com>
2023-04-17 12:11:33 +0100

    can drag Rect

--- Rect.h
+++ Rect.h
@@ -3,7 +3,7 @@
 #include "AppObject.h"
 #include "Point.h"
 
-class Rect : public AppObject
+class Rect : public AppObject // i want to call it Rectangle too..
 {
 private:
 	enum class Corner {
@@ -26,10 +26,10 @@ public:
 	Point& rightBottom() { return corners[(int)Corner::RIGHT_BOTTOM]; }
 
 
-	const Point& leftTop()	 const	{ return corners[(int)Corner::LEFT_TOP]; }
-	const Point& leftBottom()	 const	{ return corners[(int)Corner::LEFT_BOTTOM]; }
-	const Point& rightTop()	 const	{ return corners[(int)Corner::RIGHT_TOP]; }
-	const Point& rightBottom() const	{ return corners[(int)Corner::RIGHT_BOTTOM]; }
+	const Point& leftTop()	 const { return corners[(int)Corner::LEFT_TOP]; }
+	const Point& leftBottom()	 const { return corners[(int)Corner::LEFT_BOTTOM]; }
+	const Point& rightTop()	 const { return corners[(int)Corner::RIGHT_TOP]; }
+	const Point& rightBottom() const { return corners[(int)Corner::RIGHT_BOTTOM]; }
 
 	//using LT = leftTop;
 
@@ -44,25 +44,7 @@ public:
 
 
 	virtual bool onMouseEvent(const MouseEvent& ev) override {
-		if (ev.isMove()) {
-			if (dragPoint != Corner::NONE) {
-				corners[(int)dragPoint] = ev.pos;
-				return true;
-			}
-			
-			hoverPoint = Corner::NONE;
-			
-			// TODO: it is insane, change it.
-			for (int i = 0; i < 4; i++) {
-				if (corners[i].inRange(ev.pos, 3)) {
-					// isHover
-					hoverPoint = (Corner) i;
-					return true;
-				}
-			}
-
-			return false;
-		}
+		
 
 		if (ev.isLButton()) {
 			if (ev.isUp()) {
@@ -73,49 +55,70 @@ public:
 				Point& rb = rightBottom();
 
 				switch (dragPoint) {
-					case Corner::NONE: { return false; }
-					default: { return false; }
+				case Corner::NONE: { return false; }
+				default: { return false; }
 
 
-					case Corner::LEFT_TOP: {
-						lt = ev.pos;
-						setDragPoint(Corner::NONE);
-						return true;
-					}
+				case Corner::LEFT_TOP: {
+					lt = ev.pos;
+					setDragPoint(Corner::NONE);
+					return true;
+				}
 
-					case Corner::LEFT_BOTTOM: {
-						lb = ev.pos;
-						lt.x = lb.x;
-						rb.y = lb.y;
-						setDragPoint(Corner::NONE);
-						return true;
-					}
-
-
-					case Corner::RIGHT_TOP: {
-						rt = ev.pos;
-						lt.y = rt.y;
-						rb.x = rt.x;
-						setDragPoint(Corner::NONE);
-						return true;
-					}
+				case Corner::LEFT_BOTTOM: {
+					lb = ev.pos;
+					lt.x = lb.x;
+					rb.y = lb.y;
+					setDragPoint(Corner::NONE);
+					return true;
+				}
 
 
-					case Corner::RIGHT_BOTTOM: {
-						rb = ev.pos;
-						setDragPoint(Corner::NONE);
-						return true;
+				case Corner::RIGHT_TOP: {
+					rt = ev.pos;
+					lt.y = rt.y;
+					rb.x = rt.x;
+					setDragPoint(Corner::NONE);
+					return true;
+				}
 
-					}
+
+				case Corner::RIGHT_BOTTOM: {
+					rb = ev.pos;
+					setDragPoint(Corner::NONE);
+					return true;
+
+				}
 				}
 			}
-			if (ev.isDown()){
+			if (ev.isDown()) {
 				//check if any point isHovered.
+				return false;
 			}
 
+		} else if (ev.isMove()) {
+			//printf("dragPoint = %i", (int) dragPoint);
+			if (dragPoint != Corner::NONE) {
+				corners[(int)dragPoint] = ev.pos;
+				return true;
+			}
+
+			hoverPoint = Corner::NONE;
+
+			// TODO: it is insane, change it.
+			for (int i = 0; i < 4; i++) {
+				if (corners[i].inRange(ev.pos, 3)) {
+					// isHover
+					hoverPoint = (Corner)i;
+					return true;
+				}
+			}
+
+			
 		}
 
 
+		return false;
 
 
 	}
@@ -127,11 +130,10 @@ public:
 
 
 
-	void draw(HDC hdc) const override {
+	virtual void draw(HDC hdc) const override {
 		Point lt = leftTop();
 		Point rb = rightBottom();
 		::Rectangle(hdc, lt.x, lt.y, rb.x, rb.y);
-		printf("lt.x: %i, lt.y: %i, rb.x: %i, rb.y: %i\n", lt.x, lt.y, rb.x, rb.y);
 	}
 
 };
