0aa14d336d30dd521cebbae013c0801d5d330a9a
ChrisLam <fdq09eca@gmail.com>
2023-04-17 12:51:16 +0100

    updateCorner init

--- Rect.h
+++ Rect.h
@@ -41,7 +41,45 @@ public:
 		setDragPoint(Corner::RIGHT_BOTTOM);
 	}
 
+	void updateCorner(Corner c, const POINT* pos = nullptr) {
+		if (pos) {
+			corners[(int)c] = *pos;
+			return;
+		}
+		
+		
+		switch (c)
+		{
+		
+		case Rect::Corner::LEFT_TOP: {
+			leftTop().x = leftBottom().x;
+			leftTop().y = rightTop().y;
+			return;
+		}
 
+		case Rect::Corner::LEFT_BOTTOM: {
+			leftBottom().x = leftTop().x;
+			leftBottom().y = rightBottom().y; 
+			return;
+		}
+		case Rect::Corner::RIGHT_TOP: {
+			rightTop().x = rightBottom().x;
+			rightTop().y = leftTop().y;
+			return;
+		}
+		case Rect::Corner::RIGHT_BOTTOM: {
+			rightBottom().x = rightTop().x;
+			rightBottom().y = leftBottom().y;
+			return;
+		}
+		
+		default: {
+			assert("corner update failed.");
+			return;
+		}
+			
+		}
+	}
 
 	virtual bool onMouseEvent(const MouseEvent& ev) override {
 		
@@ -49,10 +87,6 @@ public:
 		if (ev.isLButton()) {
 			if (ev.isUp()) {
 
-				Point& lt = leftTop();
-				Point& lb = leftBottom();
-				Point& rt = rightTop();
-				Point& rb = rightBottom();
 
 				switch (dragPoint) {
 				case Corner::NONE: { return false; }
@@ -60,31 +94,36 @@ public:
 
 
 				case Corner::LEFT_TOP: {
-					lt = ev.pos;
+					updateCorner(Corner::LEFT_TOP, &ev.pos);
+					updateCorner(Corner::RIGHT_TOP);
+					updateCorner(Corner::LEFT_BOTTOM);
 					setDragPoint(Corner::NONE);
 					return true;
 				}
 
 				case Corner::LEFT_BOTTOM: {
-					lb = ev.pos;
-					lt.x = lb.x;
-					rb.y = lb.y;
+					updateCorner(Corner::LEFT_BOTTOM, &ev.pos);
+					updateCorner(Corner::LEFT_TOP);
+					updateCorner(Corner::RIGHT_BOTTOM);
 					setDragPoint(Corner::NONE);
 					return true;
 				}
 
 
 				case Corner::RIGHT_TOP: {
-					rt = ev.pos;
-					lt.y = rt.y;
-					rb.x = rt.x;
+					updateCorner(Corner::RIGHT_TOP, &ev.pos);
+					updateCorner(Corner::LEFT_TOP);
+					updateCorner(Corner::RIGHT_BOTTOM);
+
 					setDragPoint(Corner::NONE);
 					return true;
 				}
 
 
 				case Corner::RIGHT_BOTTOM: {
-					rb = ev.pos;
+					updateCorner(Corner::RIGHT_BOTTOM, &ev.pos);
+					updateCorner(Corner::LEFT_BOTTOM);
+					updateCorner(Corner::RIGHT_TOP);
 					setDragPoint(Corner::NONE);
 					return true;
 
@@ -134,6 +173,11 @@ public:
 		Point lt = leftTop();
 		Point rb = rightBottom();
 		::Rectangle(hdc, lt.x, lt.y, rb.x, rb.y);
+		// TODO: draw hoverPoint.
+
+		if (hoverPoint != Corner::NONE) {
+			corners[(int)hoverPoint].draw(hdc, 6);
+		}
 	}
 
 };
